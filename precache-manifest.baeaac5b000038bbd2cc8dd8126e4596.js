99e809267676682d87e1afc06fa81e6abf9df191
George <55351016+GeorgeKhgit@users.noreply.github.com>
2020-01-31 03:16:47 +0300

    Add files via upload

---
+++ precache-manifest.baeaac5b000038bbd2cc8dd8126e4596.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab0ff640194760a9a313be68ff8f6cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e7f1f0283934c6fc2eb",
+    "url": "/static/css/main.0d617933.chunk.css"
+  },
+  {
+    "revision": "a5f5fd62b84ec7d177e1",
+    "url": "/static/js/2.e78f0d92.chunk.js"
+  },
+  {
+    "revision": "3e7f1f0283934c6fc2eb",
+    "url": "/static/js/main.6f7686ae.chunk.js"
+  },
+  {
+    "revision": "d72bf6586e843ba14a38",
+    "url": "/static/js/runtime-main.976c1703.js"
+  },
+  {
+    "revision": "08a754d1d68f4671eab5e11ece77cd03",
+    "url": "/static/media/Montserrat-Regular.08a754d1.ttf"
+  },
+  {
+    "revision": "2dd0a1de870af34d48d43b7cad82b8d9",
+    "url": "/static/media/Montserrat-Regular.2dd0a1de.eot"
+  },
+  {
+    "revision": "c7b975f3e8445b8b780e6e2ddc8fdaaa",
+    "url": "/static/media/Montserrat-Regular.c7b975f3.woff2"
+  },
+  {
+    "revision": "de4a911e831ca01751ab001a54de34dd",
+    "url": "/static/media/Montserrat-Regular.de4a911e.woff"
+  },
+  {
+    "revision": "ce7d442583b55e0169cfbf18aaa7dc9b",
+    "url": "/static/media/Rectangle 67.ce7d4425.png"
+  },
+  {
+    "revision": "38675d5d2e4a071311f09a95a5c5abdc",
+    "url": "/static/media/TTSupermolotCondensed-Bold.38675d5d.woff2"
+  },
+  {
+    "revision": "a70aa4ca42f3f5925cf8347276ff34a5",
+    "url": "/static/media/TTSupermolotCondensed-Bold.a70aa4ca.ttf"
+  },
+  {
+    "revision": "cded8179376965294c31594960c84e3d",
+    "url": "/static/media/TTSupermolotCondensed-Bold.cded8179.eot"
+  },
+  {
+    "revision": "df30cff61104ae817dd793100e4d19a2",
+    "url": "/static/media/TTSupermolotCondensed-Bold.df30cff6.woff"
+  },
+  {
+    "revision": "119efddb7aa1435751eba79558e8fc09",
+    "url": "/static/media/TTSupermolotCondensed-Light.119efddb.ttf"
+  },
+  {
+    "revision": "16e87b496b24bdda2e992e741c7baa14",
+    "url": "/static/media/TTSupermolotCondensed-Light.16e87b49.woff2"
+  },
+  {
+    "revision": "48f6396e3ba90fc1fa6a80702af74650",
+    "url": "/static/media/TTSupermolotCondensed-Light.48f6396e.eot"
+  },
+  {
+    "revision": "af12771289193e131acdd0b4407cf957",
+    "url": "/static/media/TTSupermolotCondensed-Light.af127712.woff"
+  },
+  {
+    "revision": "301b533cf27aa9615a3ab0a9f8ae6cb6",
+    "url": "/static/media/TTSupermolotCondensed-Regular.301b533c.woff2"
+  },
+  {
+    "revision": "71333436ababe7f1b4cc78d18b28fd01",
+    "url": "/static/media/TTSupermolotCondensed-Regular.71333436.ttf"
+  },
+  {
+    "revision": "a3d489dcce511dc6f4bcf9998de484ef",
+    "url": "/static/media/TTSupermolotCondensed-Regular.a3d489dc.woff"
+  },
+  {
+    "revision": "edfedfcf2c8dbf5a9610c398556f67ef",
+    "url": "/static/media/TTSupermolotCondensed-Regular.edfedfcf.eot"
+  },
+  {
+    "revision": "d9755aea84c15d52e01b3b4daf0ef7cb",
+    "url": "/static/media/Technic_Regular.d9755aea.ttf"
+  }
+]);
